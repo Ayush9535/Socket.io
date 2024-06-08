1701f1418b689166831b76bc726ff1670f0257e0
Ayush9535 <144110688+Ayush9535@users.noreply.github.com>
2024-06-09 01:37:08 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { io } from "socket.io-client"
 import { useEffect, useState } from "react"
 
 function App() {
-  const socket = io("http://localhost:3000")
+  const socket = io("https://socket-io-e3s4.onrender.com")
 
   useEffect(() => {
     socket.on("connect", () => {
